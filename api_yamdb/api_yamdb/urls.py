d4232125722ff3bbf2ac6de30d7aac80bb3bbda1
Iskander <logick7@mail.ru>
2023-02-19 05:55:14 +0300

    op 3

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -17,6 +17,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+urlpatterns = [
+    path('sentry-debug/', trigger_error),
+] 
+
 urlpatterns = [
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
